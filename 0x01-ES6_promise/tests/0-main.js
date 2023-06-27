23ff785d83f1274640704d9ede50f56957e5bd5a
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-06-27 11:33:11 +0100

    task

--- 0x01-ES6_promise/tests/0-main.js
+++ 0x01-ES6_promise/tests/0-main.js
@@ -1,3 +1,6 @@
+/* eslint-disable import/extensions */
+/* eslint-disable jest/require-hook */
+/* eslint-disable import/no-unresolved */
 import getResponseFromAPI from './0-promise.js';
 
 const response = getResponseFromAPI();
---
+++ 0x01-ES6_promise/tests/2-main.js
@@ -0,0 +1,5 @@
+/* eslint-disable jest/require-hook */
+import handleResponseFromAPI from '../2-then';
+
+const promise = Promise.resolve();
+handleResponseFromAPI(promise);
