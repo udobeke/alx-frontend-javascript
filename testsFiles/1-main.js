2f903dc2670533d0a27247b8b1143493b9af1e13
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-06-27 11:32:20 +0100

    update

---
+++ testsFiles/1-main.js
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+/* eslint-disable jest/require-hook */
+import taskBlock from './1-block-scoped';
+
+console.log(taskBlock(true));
+console.log(taskBlock(false));
