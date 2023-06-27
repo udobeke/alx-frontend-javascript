f9e8b6dadd695b5c8d13b7c4f82a16ba75447784
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-06-27 11:38:59 +0100

    task

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,11 +1,10 @@
-function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
+export default function getFullResponseFromAPI(success) {
+  const arr = new Promise((resolve, reject) => {
     if (success) {
       resolve({ status: 200, body: 'Success' });
     } else {
       reject(new Error('The fake API is not working currently'));
     }
   });
+  return arr;
 }
-
-export default getFullResponseFromAPI;
--- 0x01-ES6_promise/tests/1-main.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/extensions */
-/* eslint-disable jest/require-hook */
-/* eslint-disable import/no-unresolved */
-import getFullResponseFromAPI from '../1-promise';
-
-console.log(getFullResponseFromAPI(true));
-console.log(getFullResponseFromAPI(false));
