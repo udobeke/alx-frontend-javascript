6131118cebae7f94bc4ed32aec8beba67c33c762
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-06-29 14:34:50 +0100

    task

---
+++ 0x02-ES6_classes/6-main.js
@@ -0,0 +1,7 @@
+/* eslint-disable jest/require-hook */
+import SkyHighBuilding from './6-sky_high';
+
+const building = new SkyHighBuilding(140, 60);
+console.log(building.sqft);
+console.log(building.floors);
+console.log(building.evacuationWarningMessage());
