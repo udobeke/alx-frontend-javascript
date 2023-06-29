a95d9f9cdcf3ae2fdec019f48b18a5845004eebe
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-06-29 12:34:14 +0100

    You used to attend a place like this at some point

---
+++ 0-main.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable jest/require-hook */
+import ClassRoom from './0x02-ES6_classes/0-classroom';
+
+const room = new ClassRoom(10);
+console.log(room._maxStudentsSize);
