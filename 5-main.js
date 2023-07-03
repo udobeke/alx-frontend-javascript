6e1ca63eb38dbe9251363440f82be40c9b5fad44
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-07-03 11:39:43 +0100

    test file

---
+++ 5-main.js
@@ -0,0 +1,4 @@
+/* eslint-disable jest/require-hook */
+import createInt8TypedArray from './0x03-ES6_data_manipulation/5-typed_arrays';
+
+console.log(createInt8TypedArray(10, 2, 89));
---
+++ 6-main.js
@@ -0,0 +1,4 @@
+/* eslint-disable jest/require-hook */
+import setFromArray from './0x03-ES6_data_manipulation/6-set';
+
+console.log(setFromArray([12, 32, 15, 78, 98, 15]));
