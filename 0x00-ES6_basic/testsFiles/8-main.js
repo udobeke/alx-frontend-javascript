b0d50c4ade1ecc8ab55f987fceb41279d3580b19
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-07-03 12:00:22 +0100

    fix

--- 0x00-ES6_basic/testsFiles/8-main.js
+++ 0x00-ES6_basic/testsFiles/8-main.js
@@ -1,3 +1,6 @@
-import getBudgetForCurrentYear from "./0x00-ES6_basic/8-getBudgetCurrentYear.js";
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable jest/require-hook */
+import getBudgetForCurrentYear from './8-getBudgetCurrentYear';
 
 console.log(getBudgetForCurrentYear(2100, 5200, 1090));
