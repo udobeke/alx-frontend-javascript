c6f6efc85c5db08abb18928277096a74832e1fdd
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-07-03 11:15:18 +0100

    test file

---
+++ 1-main.js
@@ -0,0 +1,6 @@
+/* eslint-disable jest/require-hook */
+import getListStudentIds from './0x03-ES6_data_manipulation/1-get_list_student_ids';
+import getListStudents from './0x03-ES6_data_manipulation/0-get_list_students';
+
+console.log(getListStudentIds('hello'));
+console.log(getListStudentIds(getListStudents()));
